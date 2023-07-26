d3fd0e84b89b1bc44cfe4898b60b0f641a6fc292
Dennis Ideler <ideler.dennis@gmail.com>
2023-07-26 11:23:52 +0100

    Format Python code
    - Sort imports (using VSCode's Python import sorter)
    - Format code (using Black code formatter)

--- graham.py
+++ graham.py
@@ -1,13 +1,14 @@
-from asyncio.log import logger
-import feedparser
-import urllib.request
-import time
-import os.path
-import html2text
-import unidecode
-import regex as re
-from htmldate import find_date
 import csv
+import os.path
+import time
+import urllib.request
+from asyncio.log import logger
+
+import feedparser
+import html2text
+import regex as re
+import unidecode
+from htmldate import find_date
 
 """
 Download a collection of Paul Graham essays in EPUB & Markdown.
@@ -30,7 +31,6 @@ if ART_NO == 1:
 
 
 def update_links_in_md(joined):
-
     matches = re.findall(b"\[\d+\]", joined)
 
     if not matches:
@@ -42,19 +42,17 @@ def update_links_in_md(joined):
             counter[0] += 1
             note_name = f"{title}_note{note_number}"
             if counter[0] == 1:
-                return bytes(f"[{note_number}](#{note_name})", 'utf-8')
+                return bytes(f"[{note_number}](#{note_name})", "utf-8")
             elif counter[0] == 2:
-                return bytes(f"<a name={note_name}>[{note_number}]</a>", 'utf-8')
-
+                return bytes(f"<a name={note_name}>[{note_number}]</a>", "utf-8")
 
         counter = [0]
 
         note_number = int(match.decode().strip("[]"))
         match_regex = match.replace(b"[", b"\[").replace(b"]", b"\]")
-        
+
         joined = re.sub(match_regex, update_links, joined)
 
-    
     return joined
 
 
@@ -70,17 +68,22 @@ for entry in reversed(rss.entries):
 
     try:
         with urllib.request.urlopen(URL) as website:
-            content = website.read().decode('unicode_escape', "utf-8")
+            content = website.read().decode("unicode_escape", "utf-8")
             parsed = h.handle(content)
             title = "_".join(TITLE.split(" ")).lower()
-            title = re.sub(r'[\W\s]+', '', title)
-            with open(f"./essays/{ART_NO:03}_{title}.md", 'wb+') as file:
+            title = re.sub(r"[\W\s]+", "", title)
+            with open(f"./essays/{ART_NO:03}_{title}.md", "wb+") as file:
                 file.write(f"# {ART_NO:03} {TITLE}\n\n".encode())
                 parsed = parsed.replace("[](index.html)  \n  \n", "")
 
-                parsed = [(p.replace("\n", " ")
-                          if re.match(r"^[\p{Z}\s]*(?:[^\p{Z}\s][\p{Z}\s]*){5,100}$", p)
-                          else "\n"+p+"\n") for p in parsed.split("\n")]
+                parsed = [
+                    (
+                        p.replace("\n", " ")
+                        if re.match(r"^[\p{Z}\s]*(?:[^\p{Z}\s][\p{Z}\s]*){5,100}$", p)
+                        else "\n" + p + "\n"
+                    )
+                    for p in parsed.split("\n")
+                ]
 
                 encoded = " ".join(parsed).encode()
                 update_with_links = update_links_in_md(encoded)
@@ -88,24 +91,23 @@ for entry in reversed(rss.entries):
 
                 print(f"✅ {ART_NO:03} {TITLE}")
 
-                with open(FILE, 'a+', newline='\n') as f:
+                with open(FILE, "a+", newline="\n") as f:
                     csvwriter = csv.writer(
-                        f,
-                        quoting=csv.QUOTE_MINIMAL,
-                        delimiter=',',
-                        quotechar='"')
+                        f, quoting=csv.QUOTE_MINIMAL, delimiter=",", quotechar='"'
+                    )
 
                     if ART_NO == 1:
                         header = ["Article no.", "Title", "Date", "URL"]
                         csvwriter.writerow(header)
 
-                    DATE = find_date(entry['link'])
+                    DATE = find_date(entry["link"])
 
                     line = [ART_NO, TITLE, DATE, URL]
 
-                    csvwriter.writerow(line) # 'list' object has no attribute 'keys'
+                    csvwriter.writerow(line)
 
     except Exception as e:
         print(f"❌ {ART_NO:03} {entry['title']}, ({e})")
+
     ART_NO += 1
     time.sleep(0.05)  # half sec/article is ~2min, be nice with servers!
